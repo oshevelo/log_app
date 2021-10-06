bde5fd79af62df9c815fcdb15a8e019fcf0270fd
maxim1106 <mrobota@livegenic.com>
2021-10-06 23:59:09 +0300

    T-1 Preparing for Messenger_(chat)_by_MaximR (Added README, JET, requirements.txt, help_commands)

--- LogApp/LogApp/urls.py
+++ LogApp/LogApp/urls.py
@@ -17,11 +17,9 @@ from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import include, path
-from messenger import views
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('chat/', include('messenger.urls')),
+    path('messenger/', include('messenger.urls')),
 ]
