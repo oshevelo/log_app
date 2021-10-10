e1b0193079c79d606b91fcd5a65a99d637d1da1d
fxmsp <andrij.shuljak@gmail.com>
2021-10-10 13:42:36 +0300

    Added models for Routes.

--- Routes/admin.py
+++ Routes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Routes
 
-# Register your models here.
+
+class RoutesAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'start_point', 'end_point', 'distance', 'time', 'rating')
+    list_display = ('id', 'start_point', 'end_point', 'distance', 'time', 'rating')
+    list_display_links = ('id',)
+    list_filter = ('distance', 'rating')
+
+
+admin.site.register(Routes, RoutesAdmin)
