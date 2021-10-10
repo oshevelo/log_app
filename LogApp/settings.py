4c5c9b21e2a1d26f76c802c3301d1b36db1dc53b
Lexazv <zvedalex@gmail.com>
2021-10-10 13:50:31 +0300

    Registered odrers app. Changed timzone

--- LogApp/settings.py
+++ LogApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
