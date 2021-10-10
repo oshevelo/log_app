e19445b2cb46c8b1020712c9aab692c577d193f2
MaximRobota <Maxim11061106@gmail.com>
2021-10-10 19:54:03 +0300

    Fix models

--- messenger/models.py
+++ messenger/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
-from django.db.models import Model, TextField, DateTimeField, ForeignKey, CASCADE
+from django.db import models
 
 
-class MessageModel(Model):
-    user = ForeignKey(User, on_delete=CASCADE, verbose_name='user', related_name='from_user', db_index=True)
-    recipient = ForeignKey(User, on_delete=CASCADE, verbose_name='recipient', related_name='to_user', db_index=True)
-    timestamp = DateTimeField('timestamp', auto_now_add=True, editable=False, db_index=True)
-    body = TextField('body', max_length=20000)
+class MessageModel(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='user', related_name='from_user', db_index=True
+    )
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='recipient', related_name='to_user', db_index=True
+    )
+    timestamp = models.DateTimeField('timestamp', auto_now_add=True, editable=False, db_index=True)
+    body = models.TextField('body', max_length=20000)
 
     def __str__(self):
         return str(self.id)
