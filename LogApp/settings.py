3a5bd57054fd8673a4751bb946ca07c2df685a3d
Alex Shevelo <alex.shevelo@gmail.com>
2021-11-07 17:49:41 +0200

    abstract models demon

--- LogApp/settings.py
+++ LogApp/settings.py
@@ -132,4 +132,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 from .local_settings import *
