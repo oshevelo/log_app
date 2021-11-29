106b2f87f0230385d456820c31d981dd95ec64d9
fxmsp <andrij.shuljak@gmail.com>
2021-11-30 01:41:07 +0200

    Refactoring names of vars in admin.

--- Route/admin.py
+++ Route/admin.py
@@ -3,10 +3,12 @@ from .models import Route
 
 
 class RouteAdmin(admin.ModelAdmin):
-    search_fields = ('id', 'start_point_x', 'start_point_y', 'end_point_x', 'end_point_y', 'distance', 'date_time', 'rating')
-    list_display = ('id', 'start_point_x', 'start_point_y', 'end_point_x', 'end_point_y', 'distance', 'date_time', 'rating')
+    search_fields = ('id', 'start_point_lat', 'start_point_long', 'end_point_lat', 'end_point_long',
+                     'journey_dist', 'journey_time', 'journey_rating')
+    list_display = ('id', 'start_point_lat', 'start_point_long', 'end_point_lat', 'end_point_long',
+                    'journey_dist', 'journey_time', 'journey_rating')
     list_display_links = ('id',)
-    list_filter = ('distance', 'rating')
+    list_filter = ('journey_dist', 'journey_rating')
 
 
 admin.site.register(Route, RouteAdmin)
