183db98ce61eadecaa67d73fe8e920fb4d94a575
fxmsp <andrij.shuljak@gmail.com>
2021-11-29 23:45:42 +0200

    Changing names of variables in model Route.

--- Route/models.py
+++ Route/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Route(models.Model):
-    name = models.TextField()
-    start_point_x = models.DecimalField(max_digits=10, decimal_places=10)
-    start_point_y = models.DecimalField(max_digits=10, decimal_places=10)
-    end_point_x = models.DecimalField(max_digits=10, decimal_places=10)
-    end_point_y = models.DecimalField(max_digits=10, decimal_places=10)
-    distance = models.IntegerField()  # min_dist | max_dist
-    date_time = models.DateTimeField()  # time to
-    rating = models.IntegerField()  #
+    name_route = models.TextField()
+    start_point_lat = models.DecimalField(max_digits=10, decimal_places=10)
+    start_point_long = models.DecimalField(max_digits=10, decimal_places=10)
+    end_point_lat = models.DecimalField(max_digits=10, decimal_places=10)
+    end_point_long = models.DecimalField(max_digits=10, decimal_places=10)
+    journey_dist = models.IntegerField()
+    journey_time = models.DateTimeField()
+    journey_rating = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.name_route
