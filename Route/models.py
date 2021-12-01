188633dacc4e806addff4e3a12e180f18b4e85a4
fxmsp <andrij.shuljak@gmail.com>
2021-12-01 13:48:37 +0200

    start and end points have been changed.

--- Route/models.py
+++ Route/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Route(models.Model):
     name_route = models.TextField()
-    start_point_lat = models.DecimalField(max_digits=10, decimal_places=10)
-    start_point_long = models.DecimalField(max_digits=10, decimal_places=10)
-    end_point_lat = models.DecimalField(max_digits=10, decimal_places=10)
-    end_point_long = models.DecimalField(max_digits=10, decimal_places=10)
+    start_point_lat = models.DecimalField(max_digits=16, decimal_places=14)
+    start_point_long = models.DecimalField(max_digits=16, decimal_places=14)
+    end_point_lat = models.DecimalField(max_digits=16, decimal_places=14)
+    end_point_long = models.DecimalField(max_digits=16, decimal_places=14)
     journey_dist = models.IntegerField()
     journey_time = models.DateTimeField()
     journey_rating = models.IntegerField()
