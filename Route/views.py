11c89929f92b6818b0155fe28f728d43fd1e25f1
fxmsp <andrij.shuljak@gmail.com>
2021-12-01 13:50:28 +0200

    Making views for Route.

--- Route/views.py
+++ Route/views.py
@@ -3,6 +3,7 @@
 
 from rest_framework import pagination
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Route
 from .serializers import RouteListSerializer, RouteDetailsSerializer
@@ -13,6 +14,16 @@ class RouteList(generics.ListCreateAPIView):
     pagination_class = pagination.LimitOffsetPagination
     serializer_class = RouteListSerializer
 
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return Route.objects.filter(respondent=self.request.user)
+        return Route.objects.all()
+
 
 class RouteDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RouteDetailsSerializer
+
+    def get_object(self):
+        if not self.request.user.is_superuser:
+            get_object_or_404(Route, pk=self.kwargs.get('route_id'), respondent=self.request.user)
+        return get_object_or_404(Route, pk=self.kwargs.get('route_id'))
