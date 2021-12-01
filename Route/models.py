f725b75de341c3ff489bf48e3cdd96272ef10ea5
fxmsp <andrij.shuljak@gmail.com>
2021-12-01 13:55:26 +0200

    2 before and 15 after digits

--- Route/models.py
+++ Route/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Route(models.Model):
     name_route = models.TextField()
-    start_point_lat = models.DecimalField(max_digits=16, decimal_places=14)
-    start_point_long = models.DecimalField(max_digits=16, decimal_places=14)
-    end_point_lat = models.DecimalField(max_digits=16, decimal_places=14)
-    end_point_long = models.DecimalField(max_digits=16, decimal_places=14)
+    start_point_lat = models.DecimalField(max_digits=17, decimal_places=15)
+    start_point_long = models.DecimalField(max_digits=17, decimal_places=15)
+    end_point_lat = models.DecimalField(max_digits=17, decimal_places=15)
+    end_point_long = models.DecimalField(max_digits=17, decimal_places=15)
     journey_dist = models.IntegerField()
     journey_time = models.DateTimeField()
     journey_rating = models.IntegerField()
