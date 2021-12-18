9e4d017505b26b24b6496c655ab6bd2ac314eae6
maxim1106 <mrobota@livegenic.com>
2021-12-18 22:10:50 +0200

    Added conf for nginx

--- LogApp/settings.py
+++ LogApp/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 REST_FRAMEWORK = {
